f86cc603a44bed42f7473563f407acbd0a601729
Mansoor-Muhammad-ICD2O1-01 <mansoor.muhammad@student.tdsb.on.ca>
2024-10-15 14:33:59 +0000

    764747

--- PP2_2.py
+++ PP2_2.py
@@ -1,25 +1,41 @@
 
 
 def q1(): 
-  #Write Assignment code here
+  men = int(input("Input a interger: "))
+  if men == 5:
+    print("The number is 5")
+  else: 
+    print("The number is not 5") 
 
 
 def q2(): 
-  #Write Assignment code here
-
+  won = float(input("Input a number: "))
+  if won > 0:
+    print("Positive")
+  else:
+    print("Negative")
 def q3(): 
-  #Write Assignment code here
-
+  one = int(input("Input and interger: "))
+  if one % 2 == 0:
+    print("Even")
+  else: 
+    print("Odd")
 def q4(): 
-  #Write Assignment code here
+  one = input("Input Hello: ")
+  if one == "Hello":
+    print("The word is Hello")
+  else: 
+    print("The word is not Hello")
+
 
 
 
 
 #Do not alter the following code
 #Comment out the following code when running your tests
-
+'''
 q1()
 q2()
 q3()
 q4()
+'''
